65fe00ad0424c80488a98dce9bd22aef85839702
bikrantj <160575554+bikrantj@users.noreply.github.com>
2024-07-22 21:43:45 +0545

    feat: update generate random number fn

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const generateRandomNumber = () =>{
-    return Math.floor(Math.random() * 1000);
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
